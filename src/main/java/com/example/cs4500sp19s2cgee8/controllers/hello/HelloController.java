cf3ae2b49bcf19c4e6c421e798b5b563b20e47a5
gee-christine <christinegee8@gmail.com>
2019-01-10 18:53:43 -0500

    first commit

---
+++ src/main/java/com/example/cs4500sp19s2cgee8/controllers/hello/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.cs4500sp19s2cgee8.controllers.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/api/hello/string")
+    public String sayHello() {
+        return "Hello CS4500!";
+    }
+    @RequestMapping("api/hello/object")
+    public HelloObject sayHelloObject() {
+        HelloObject obj = new HelloObject("Hello Object CS4500!");
+        return obj;
+    }
+}
---
+++ src/main/java/com/example/cs4500sp19s2cgee8/controllers/hello/HelloObject.java
@@ -0,0 +1,16 @@
+package com.example.cs4500sp19s2cgee8.controllers.hello;
+
+public class HelloObject {
+    private String message;
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public HelloObject(String message) {
+        this.message = message;
+    }
+    public HelloObject() {
+    }
+}
