30ceca24a4db4e09bf2e17500156c52d64a10ef6
gee-christine <christinegee8@gmail.com>
2019-01-22 18:42:31 -0500

    implemented findUserById method

--- src/main/java/com/example/cs4500sp19s2cgee8/services/UserService.java
+++ src/main/java/com/example/cs4500sp19s2cgee8/services/UserService.java
@@ -2,6 +2,7 @@ package com.example.cs4500sp19s2cgee8.services;
 
 import com.example.cs4500sp19s2cgee8.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,12 +13,24 @@ public class UserService {
     static List<User> users = new ArrayList();
 
     static {
+        users.add(new User(456, "bob", "bob", "Bob", "Marley"));
         users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
         users.add(new User(234, "bob", "bob", "Bob", "Marley"));
+
     }
 
     @GetMapping("/api/user")
     public List<User> findAllUsers() {
         return users;
+    }
+
+    @GetMapping("api/user/{userId}")
+    public User findUserById(@PathVariable("userId") Integer id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
     }
 }
