84336c83ba01ad1bdeda77df6d7d219020250705
gee-christine <christinegee8@gmail.com>
2019-01-22 17:38:35 -0500

    implement User model with getters/setters

---
+++ src/main/java/com/example/cs4500sp19s2cgee8/model/User.java
@@ -0,0 +1,44 @@
+package com.example.cs4500sp19s2cgee8.model;
+
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    // provide a public set and get method for each of the class variables above
+    // make sure to use proper naming conventions for Java methods, e.g.,
+    // methods should be camelcase. Here's an example using the firstName variable:
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+}
